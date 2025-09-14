03368ffbe3245715d9c5d8d4bdce812f6d4f24e1
riyaan-bai <riyaanb2306@gmail.com>
2025-09-15 06:34:45 +0700

    Perubahan pada domain

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "riyaan-baihaqi-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://pbp.cs.ui.ac.id/riyaan.baihaqi/footballnews"
+    "https://riyaan-baihaqi-footballnews.pbp.cs.ui.ac.id/"
 ]
 
 # Application definition
